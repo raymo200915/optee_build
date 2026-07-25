0c16778b9ed6b4cad7e063c9872cc1c9fb17db9e
Raymond Mao <raymond.mao@riscstar.com>
2026-07-25 10:28:30 -0400

    [TEST] ci: continue to test when failed and print test number for qemu risc-v
    Print each test number and continue to test when failed for QEMU
    RISC-V.
    This is to get all test cases tested at current stage and avoid the
    check due to:
    Kernel error: 'rcu: INFO: rcu_sched detected stalls'.
    
    Signed-off-by: Raymond Mao <raymond.mao@riscstar.com>

--- qemu_riscv64.mk
+++ qemu_riscv64.mk
@@ -309,6 +309,7 @@ CHECK_DEPS := all
 endif
 
 CHECK_TESTS ?= xtest
+XTEST_CONTINUE_ON_FAIL ?= n
 ifneq ($(TIMEOUT),)
 check-args := --timeout $(TIMEOUT)
 endif
@@ -317,6 +318,9 @@ check-args += --tests $(CHECK_TESTS)
 endif
 ifneq ($(XTEST_ARGS),)
 check-args += --xtest-args "$(XTEST_ARGS)"
+endif
+ifeq ($(XTEST_CONTINUE_ON_FAIL),y)
+check-args += --continue-on-fail
 endif
 
 QEMU_CHECK_ARGS = $(QEMU_BASE_ARGS)
